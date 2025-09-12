eea3fee88d4f78c403bae1afeb28c028d9bfa99f
Ahmed Naveed <ahmedbinnaveed123@gmail.com>
2025-09-13 02:08:47 +0500

    profile + edit profile  + highlights

--- app/src/main/java/com/ahmed/i221132/HomeActivity.kt
+++ app/src/main/java/com/ahmed/i221132/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +17,8 @@ class HomeActivity : AppCompatActivity() {
 
         val search_image = findViewById<ImageView>(R.id.search_image)
         val message_button = findViewById<ImageView>(R.id.message_button)
+        val profile_image = findViewById<ImageView>(R.id.profile_image)
+        val add_post = findViewById<ImageView>(R.id.add_post)
 
         search_image.setOnClickListener {
             val intent = Intent(this, search::class.java)
@@ -24,6 +28,28 @@ class HomeActivity : AppCompatActivity() {
         message_button.setOnClickListener {
             val intent = Intent(this, message::class.java)
             startActivity(intent)
+        }
+
+        profile_image.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+
+        }
+
+        var launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            if(it.resultCode == RESULT_OK){
+                var uri = it.data?.data
+                add_post.setImageURI(uri)
+            }
+        }
+
+        add_post.setOnClickListener {
+            var intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            launcher.launch(intent)
         }
 
     }
